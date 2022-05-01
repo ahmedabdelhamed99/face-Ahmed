ac546c24003ec6a66b7f8bbc4d76a95dcd1c2104
ahmedabdelhamed99 <103907919+ahmedabdelhamed99@users.noreply.github.com>
2022-05-01 13:52:30 +0200

    Add files via upload

--- assets/js/book.js
+++ assets/js/book.js
@@ -332,4 +332,19 @@ function updateVisitCount() {
 	})
 }
 
-/*--down--*/
+/*--read more--*/
+function myFunction() {
+  var dots = document.getElementById("dots");
+  var moreText = document.getElementById("more");
+  var btnText = document.getElementById("myBtn");
+
+  if (dots.style.display === "none") {
+    dots.style.display = "inline";
+    btnText.innerHTML = "أقرأ المزيد";
+    moreText.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    btnText.innerHTML = " أقرأ أقل";
+    moreText.style.display = "inline";
+  }
+}
